3891e5e0225d06594e0b8dd6bc08ac78909585b5
agungTuanay <agung.tuanany@gmail.com>
2020-05-17 14:25:19 +0700

    Chapter_5: Course project basic | Adding a dummy data

--- project_2/src/App.svelte
+++ project_2/src/App.svelte
@@ -1,5 +1,26 @@
 <script>
     import Header from "./UI/Header.svelte"
+
+    const meetups = [
+        {
+            id              : "meetup1",
+            title           : "Learn to code",
+            description     : "In this meetup, we will have some exprets talks about front-end development",
+            imageUrl        : "",
+            time            : "16:00 GMT",
+            address         : "27th Munchies Roead, 22441 New Mark"
+            contactEmail    : "code@test.com"
+        },
+        {
+            id              : "meetup2",
+            title           : "Learn to code",
+            description     : "In this meetup, we talk about back-end development",
+            imageUrl        : "",
+            time            : "16:00 GMT",
+            address         : "27th Munchies Roead, 22441 New Mark"
+            contactEmail    : "codeExpress@test.com"
+        },
+    ]
 </script>
 
 <style>
