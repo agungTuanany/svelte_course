c190031f8331a91e3185178fe0d32cdfb0cbca1a
agungTuanay <agung.tuanany@gmail.com>
2020-05-20 05:28:51 +0700

    Chapter_7: Course Project Component Deep Dive | Creating and edit Meetup component

--- project_2/src/App.svelte
+++ project_2/src/App.svelte
@@ -3,15 +3,9 @@
     import MeetupGrid   from "./Meetups/MeetupGrid.svelte"
     import TextInput    from "./UI/TextInput.svelte"
     import Button       from "./UI/Button.svelte"
+    import EditMeetup   from "./Meetups/EditMeetup.svelte"
 
     // ####################################################
-    let title       = ""
-    let subtitle    = ""
-    let time        = ""
-    let imageUrl    = ""
-    let address     = ""
-    let email       = ""
-    let description = ""
 
 
     let meetups = [
@@ -38,6 +32,8 @@
             isFavorite      : false
         },
     ]
+
+    let editMode = null
 
     // ####################################################
 
@@ -79,58 +75,14 @@
         margin-top: 5rem;
     }
 
-    form {
-        width: 30rem;
-        max-width: 90%;
-        margin: auto;
-    }
 </style>
 
 <Header/>
 <main>
-    <form on:submit|preventDefault="{addMeetup}" action="" method="post">
-        <TextInput
-            id="title"
-            label="title"
-            value="{title}"
-            on:input={event => {title = event.target.value}} />
-        <TextInput
-            id="subtitle"
-            label="subtitle"
-            value="{subtitle}"
-            on:input={event => {subtitle = event.target.value}} />
-        <TextInput
-            id="time"
-            label="time"
-            value="{time}"
-            on:input={event => {time = event.target.value}} />
-        <TextInput
-            id="imageUrl"
-            label="imageUrl"
-            value="{imageUrl}"
-            on:input={event => {imageUrl = event.target.value}} />
-        <TextInput
-            id="address"
-            label="address"
-            value="{address}"
-            on:input={event => {address = event.target.value}} />
-        <TextInput
-            id="email"
-            label="E-mail"
-            type="email"
-            value="{email}"
-            on:input={event => {email = event.target.value}} />
-        <TextInput
-            id="description"
-            label="description"
-            value="{description}"
-            rows=4
-            cols=8
-            controlType="textarea"
-            on:input={event => {description = event.target.value}} />
-        <Button type="submit" caption="Save" />
-    </form>
-    <!-- <MeetupGrid meetup="{meetup}" /> -->
+    <Button caption="New Meetup" on:click={() => editMode = "add"} />
+    {#if editMode === "add"}
+        <EditMeetup />
+    {/if}
     <MeetupGrid {meetups} on:toggle-fav="{toggleFavorite}"/>
 
 </main>
---
+++ project_2/src/Meetups/EditMeetup.svelte
@@ -0,0 +1,95 @@
+<script>
+    import { createEventDispatcher } from "svelte"
+
+    import TextInput from "./../UI/TextInput.svelte"
+    import Button    from "./../UI/Button.svelte"
+
+    // ############################################
+    // Props
+    let title       = ""
+    let subtitle    = ""
+    let time        = ""
+    let imageUrl    = ""
+    let address     = ""
+    let email       = ""
+    let description = ""
+
+    const submitForm = () => {
+        dispatch ("save", {
+            title           : title,
+            subtitle        : subtitle,
+            time            : time,
+            address         : address,
+            email           : email,
+            description     : description,
+            imageUrl        : imageUrl
+        })
+    }
+
+    const dispatch = createEventDispatcher ()
+
+
+</script>
+
+<style>
+
+    form {
+        width: 30rem;
+        max-width: 90%;
+        margin: auto;
+    }
+
+</style>
+
+
+
+
+<form on:submit|preventDefault="{submitForm}" action="" method="post">
+    <TextInput
+        id="title"
+        label="title"
+        value="{title}"
+        on:input={event => {title = event.target.value}}
+    />
+    <TextInput
+        id="subtitle"
+        label="subtitle"
+        value="{subtitle}"
+        on:input={event => {subtitle = event.target.value}}
+    />
+    <TextInput
+        id="time"
+        label="time"
+        value="{time}"
+        on:input={event => {time = event.target.value}}
+    />
+    <TextInput
+        id="imageUrl"
+        label="imageUrl"
+        value="{imageUrl}"
+        on:input={event => {imageUrl = event.target.value}}
+    />
+    <TextInput
+        id="address"
+        label="address"
+        value="{address}"
+        on:input={event => {address = event.target.value}}
+    />
+    <TextInput
+        id="email"
+        label="E-mail"
+        type="email"
+        value="{email}"
+        on:input={event => {email = event.target.value}}
+    />
+    <TextInput
+        id="description"
+        label="description"
+        value="{description}"
+        rows=4
+        cols=8
+        controlType="textarea"
+        on:input={event => {description = event.target.value}}
+    />
+    <Button type="submit" caption="Save" />
+</form>
