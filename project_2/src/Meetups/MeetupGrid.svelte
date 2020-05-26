db1a1d65c1f95b2f4135dd27387eba76c2539c6d
agungTuanay <agung.tuanany@gmail.com>
2020-05-26 12:29:29 +0700

    Chapter_11: Course Project Store | Adding a filter switch

--- project_2/src/Meetups/MeetupGrid.svelte
+++ project_2/src/Meetups/MeetupGrid.svelte
@@ -1,28 +1,47 @@
 <script>
     // Buildin dependencies
     import MeetupItem from "./MeetupItem.svelte"
+    import MeetupFilter from "./MeetupFilter.svelte"
 
     // props
     export let meetups;
+
+    let favsOnly = false
+
+    // Reactive statement
+    $: filteredMeetups = favsOnly ?  meetups.filter (m => m.isFavorite) : meetups
+
+    // Instant function
+    function setFilter (event) {
+        favsOnly = event.detail === 1
+    }
 </script>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         grid-gap: 1rem;
     }
 
+    #meetup-controls {
+        margin: 1rem;
+    }
+
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
 </style>
 
+<section class="meetup-controls">
+    <MeetupFilter on:select="{setFilter}"/>
+</section>
+
 <section id="meetups">
-    {#each meetups as meetup}
+    {#each filteredMeetups as meetup}
         <MeetupItem
             id="{meetup.id}"
             title="{meetup.title}"
