e8a44c8448819c129376f0e13718257d64bac9b7
agungTuanay <agung.tuanany@gmail.com>
2020-05-26 19:38:38 +0700

    Chapter_12: Motion Transition & Animation | tweened store & custom interpolator

---
+++ motion_transition_and_animation/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { writable } from "svelte/store"
+    import { tweened } from "svelte/motion"
+    import { cubicIn } from "svelte/easing"
+
+    // const progress = writable (0)
+    const progress = tweened (0, {
+        delay           : 0,
+        duration        : 700,
+        easing          : cubicIn,
+    })
+
+    setTimeout (() => {
+        progress.set (0.5)
+    }, 1500)
+
+
+</script>
+
+
+<style>
+</style>
+
+<main>
+    <progress value={$progress}></progress>
+</main>
