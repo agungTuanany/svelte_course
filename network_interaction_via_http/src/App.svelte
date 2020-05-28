6a2745a884f9cdd244a9040c21f533e36529092b
agungTuanay <agung.tuanany@gmail.com>
2020-05-28 19:31:31 +0700

    Chapter_14: Network Interaction via HTTP | sending data via a post request to firebase

---
+++ network_interaction_via_http/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+    let hobbies = []
+    let hobbyInput
+
+    function addHobby () {
+        hobbies = [...hobbies, hobbyInput.value]
+
+        fetch ("https://svelte-course-57736.firebaseio.com/hobbies.json", {
+            method          : "POST",
+            body            : JSON.stringify (hobbies),
+            headers         : {
+                "Content-Type"      : "application/json"
+            }
+
+        }).then (res => {
+            if (!res.ok) {
+                throw new Error ("Responds Failed!")
+            }
+            // .....
+        }).catch (err => {
+            console.log (err)
+
+        })
+    }
+</script>
+
+<style>
+</style>
+
+
+<main>
+    <label for="hobby">Hobby</label>
+    <input type="text" id="hoby" bind:this={hobbyInput}>
+    <button on:click={addHobby}>Add</button>
+
+    <ul>
+        {#each hobbies as hobby}
+            <li>{hobby}</li>
+        {/each}
+    </ul>
+</main>
+
