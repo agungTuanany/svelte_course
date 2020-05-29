609750f90a6404d0ab51d758c208633549134cc6
agungTuanay <agung.tuanany@gmail.com>
2020-05-30 04:17:54 +0700

    Chapter_16: Special Element | Dynamic Component

---
+++ special_element/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Product from "./Product.svelte"
+    import CartItem from "./CartItem.svelte"
+
+    let renderedComponent = { cmp: Product, title: "Test Product", id: "p1" }
+
+    function toggle () {
+        if (renderedComponent.cmp === Product) {
+            renderedComponent = { cmp: CartItem, title: "Another test Product", id: "p2" }
+
+        }
+        else {
+            renderedComponent = { cmp:Product, title: "Test Product", id: "p1" }
+        }
+    }
+</script>
+
+
+<style>
+</style>
+
+<main>
+    <button on:click={toggle}>Toggle Display</button>
+    <svelte:component this={renderedComponent.cmp} title={renderedComponent.title} id={renderedComponent.id} />
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
