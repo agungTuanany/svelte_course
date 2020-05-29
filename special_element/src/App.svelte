dde24e164c30fd8be787a6310b68c86d4cbcdef2
agungTuanay <agung.tuanany@gmail.com>
2020-05-30 05:01:12 +0700

    Chapter_16: Recursive Component

--- special_element/src/App.svelte
+++ special_element/src/App.svelte
@@ -1,6 +1,30 @@
 <script>
     import Product from "./Product.svelte"
     import CartItem from "./CartItem.svelte"
+    import FamilyNode from "./FamilyNode.svelte"
+
+    // Tree component
+    let familyStructure = [
+        {
+            isParent    : true,
+            name        : "Chris",
+            children    : [
+                {
+                    isParent    : true,
+                    name        : "Moe",
+                    children    : [
+                        {
+                            isParent    : false,
+                            name        : "Jule"
+                        }
+                    ]
+                }
+            ]
+        },
+        { isParent: false, name: "Anna" }
+    ]
+
+    let showProduct = true
 
     let renderedComponent = { cmp: Product, title: "Test Product", id: "p1" }
 
@@ -13,6 +37,10 @@
             renderedComponent = { cmp:Product, title: "Test Product", id: "p1" }
         }
     }
+
+    function oriToggle () {
+        showProduct = !showProduct
+    }
 </script>
 
 
@@ -20,7 +48,27 @@
 </style>
 
 <main>
+    <h1>Dynamic component</h1>
     <button on:click={toggle}>Toggle Display</button>
     <svelte:component this={renderedComponent.cmp} title={renderedComponent.title} id={renderedComponent.id} />
+    <hr>
+
+    <!-- static component -->
+    <h1>static component</h1>
+    <button on:click={oriToggle}>Toogle Display</button>
+    {#if showProduct}
+        <Product title="Original Product" id="p1" />
+    {:else}
+        <CartItem title="Original CartItem" id= "p2" />
+    {/if}
+
+    <hr>
+
+    {#each familyStructure as familyMember}
+        <FamilyNode member={familyMember} />
+
+    {/each}
+
+
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
---
+++ special_element/src/FamilyNode.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let member
+</script>
+<!-- NOTE: how to render familyNode from inside familyNode
+    Recursive component is really useful if rendering such a folder structure or
+    whenever have a dependencies on self.
+-->
+
+
+<h1>{member.name}</h1>
+{#if member.isParent}
+    <ul>
+        {#each member.children as child}
+            <!-- <li>{child.name}</li> -->
+            <li>
+                <svelte:self member={child} />
+            </li>
+        {/each}
+    </ul>
+{/if}
