19ca5062551cbd29a27240c322eaed6e5d3605f9
agungTuanay <agung.tuanany@gmail.com>
2020-05-30 16:08:20 +0700

    Chapter_17: Routing SSR with Sapper | How preloading and data fetching works

--- sapper_project/src/routes/blog/[slug].svelte
+++ sapper_project/src/routes/blog/[slug].svelte
@@ -1,5 +1,6 @@
 <script context="module">
     export async function preload({ params, query }) {
+        console.log ("Preloading a single post!..")
         // the `slug` parameter is available because
         // this file is called [slug].svelte
         const res = await this.fetch(`blog/${params.slug}.json`);
--- sapper_project/src/routes/blog/index.svelte
+++ sapper_project/src/routes/blog/index.svelte
@@ -1,13 +1,39 @@
 <script context="module">
+    /*
+    * this.fetch() is provided by sappers work on server and client side
+    * preload() can run both in server and client side
+    */
     export function preload({ params, query }) {
+        console.log ("Preloading....")
         return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-            return { posts };
+            return { posts: posts };
         });
     }
 </script>
 
 <script>
+    import { onMount } from "svelte"
+
     export let posts;
+
+    /*
+     * This preload on server side is happen on purpose because need to hydrate pre-rendered DOM
+     * which is ship from the server to the client side,
+     *
+     * fetch() is a (API) function build into JavaScript only in client side (browser) not into nodeJS.
+     * Cause on nodeJS there is no fetch function.
+     */
+
+
+    // This preload run on server and client side
+    console.log ("Executed from server")
+
+
+    onMount (() => {
+        // onMount only run on client side
+        // fetch ()
+        console.log ("mounted into client side only")
+    })
 </script>
 
 <style>
