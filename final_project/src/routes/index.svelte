017956f960b0622254d1b527a8db915f4c0b4b58
agungTuanay <agung.tuanany@gmail.com>
2020-05-30 23:07:31 +0700

    ChapteR_18: Final Project | prefetching data on the server

--- final_project/src/routes/index.svelte
+++ final_project/src/routes/index.svelte
@@ -1,3 +1,38 @@
+<script context="module">
+    export function preload (page) {
+        //console.log ("preload function: ", page)
+        return this.fetch ("https://svelte-course-57736.firebaseio.com/meetups.json")
+            .then (res => {
+                if (!res.ok) {
+                    throw new Error ("Fetching data from server failed")
+                }
+                return res.json ()
+            })
+            .then (data => {
+                const loadedMeetups = []
+                for (const key in data) {
+                    loadedMeetups.push ({
+                        ...data [key],
+                        id: key
+                    })
+                }
+                return {fetchedMeetups: loadedMeetups}
+
+                // For Development environment
+                //setTimeout (() => {
+                //    isLoading = false
+                //    meetups.setMeetups (loadedMeetups.reverse ())
+                //} , 1000)
+            })
+            .catch (err => {
+                error = err
+                isLoading = false
+                console.log (err)
+                this.error (500, "Could not fetch meetups from firebase!!")
+            })
+
+    }
+</script>
 <script>
     // Dependencies
     import { createEventDispatcher, onMount, onDestroy } from "svelte"
@@ -15,14 +50,13 @@
 
     // ################################################
     // props
-    export let fetchedMeetups = []
+    export let fetchedMeetups
 
     const dispatch = createEventDispatcher ()
     let favsOnly = false
     let editMode
     let editedId
-    let isLoading = true
-    let unsubscribe
+    let isLoading
 
     // Reactive statement
     $: filteredMeetups = favsOnly ?  fetchedMeetups.filter (m => m.isFavorite) : fetchedMeetups
@@ -31,41 +65,8 @@
     // ################################################
     // Instant function
 
-    // Fetch data on the client (DOM)
     onMount (() => {
-        unsubscribe = meetups.subscribe (items => fetchedMeetups = items)
-        fetch ("https://svelte-course-57736.firebaseio.com/meetups.json")
-            .then (res => {
-                if (!res.ok) {
-                    throw new Error ("Fetching data from server failed")
-                }
-                return res.json ()
-            })
-            .then (data => {
-                const loadedMeetups = []
-                for (const key in data) {
-                    loadedMeetups.push ({
-                        ...data [key],
-                        id: key
-                    })
-                }
-                // For Development environment
-                setTimeout (() => {
-                    isLoading = false
-                    meetups.setMeetups (loadedMeetups.reverse ())
-                } , 1000)
-            })
-            .catch (err => {
-                error = err
-                isLoading = false
-                console.log (err)
-            })
-    })
-
-    onDestroy (() => {
-        if (unsubscribe) {
-            unsubscribe ()
-        }
+        meetups.setMeetups (fetchedMeetups)
     })
 
     function setFilter (event) {
@@ -115,15 +116,11 @@
 </style>
 
 <svelte:head>
-    <title>ALl Meetups</title>
+    <title>All Meetups</title>
 </svelte:head>
 
 {#if editMode === "edit"}
-    {#if error}
-        <Error message={error.message} on:cancel={clearError} />
-    {:else}
-        <EditMeetup id="{editedId}" on:save="{savedMeetup}" on:cancel="{cancelEdit}"/>
-    {/if}
+    <EditMeetup id="{editedId}" on:save="{savedMeetup}" on:cancel="{cancelEdit}"/>
 {/if}
 {#if isLoading}
     <LoadingSpinner />
